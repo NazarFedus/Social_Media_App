caaaf6b8cf1457def76f2746efcae5b4c6bba6b5
NazarFedus <simpleprograming7@gmail.com>
2023-05-23 16:41:57 +0300

    add app structure

---
+++ client/src/components/Posts/Posts.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import Post from "./Post/Post";
+
+const Posts: FC = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
