7130282ceaab906eb358c6458285fd90fe201ad1
NazarFedus <simpleprograming7@gmail.com>
2023-05-26 23:56:50 +0300

    update reducer logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import { useDispatch } from "react-redux";
+
+import {getPosts} from './actions/posts'
 import memories from "./img/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
@@ -8,6 +12,11 @@ import useStyles from './styles'
 
 const App: FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch])
 
   return (
     <Container maxWidth="lg">
@@ -21,7 +30,7 @@ const App: FC = () => {
         <Container>
           <Grid
             container
-            justify="space-between"
+            justifyContent="space-between"
             alignItems="stretch"
             spacing={3}
           >
---
+++ client/src/actions/posts.ts
@@ -0,0 +1,9 @@
+import * as api from '../api';
+
+// Actions
+
+const getPosts = () => async (dispatch) => {
+     const action = {type: 'FETCH__ALL', payload: []}
+
+     return action
+}
